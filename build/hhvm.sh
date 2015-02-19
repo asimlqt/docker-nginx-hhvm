5986636b4bc0179e8801f688a860da7f20e6e3a8
Asim Liaquat <asimlqt22@gmail.com>
2015-02-19 20:52:02 +0000

    Fixed error reporting

--- build/hhvm.sh
+++ build/hhvm.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/usr/bin/hhvm --mode daemon -vServer.Type=fastcgi -vServer.Port=9000 &
+/usr/bin/hhvm --mode server -vServer.Type=fastcgi -vServer.Port=9000 -vLog.Level=Error -vLog.NoSilencer=on -vLog.Header=on -vLog.UseLogFile=true -vLog.File=/var/log/hhvm/error.log -vLog.AlwaysLogUnhandledExceptions=true
